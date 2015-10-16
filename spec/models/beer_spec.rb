a65c509bb6578b1e404fdcbd70f29fd59cfe6be1
Samuel Monroe <spat.monroe@gmail.com>
2015-10-16 12:38:05 +0200

    Tests sur le modèle Beer

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,5 +1,41 @@
+# == Schema Information
+#
+# Table name: beers
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  degree      :integer
+#  description :text
+#  story       :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Beer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = { :name => "Taras Boulba", :degree => 4.5}
+  end
+
+  it "devrait créer une instance dotée des attributs valides" do
+    Beer.create!(@attr)
+  end
+
+  it "exige un nom" do
+    unnamed_beer = Beer.new(@attr.merge(:name => ""))
+    expect(unnamed_beer).to_not be_valid
+  end
+
+  it "rejette les noms trop longs" do
+    too_long_name = "b"*51
+    too_long_beername = Beer.new(@attr.merge(:name => too_long_name))
+    expect(too_long_beername).to_not be_valid
+  end
+
+  it "exige un titrage en alcool" do
+    no_titling_beer = Beer.new(@attr.merge(:degree => nil))
+    expect(no_titling_beer).to_not be_valid
+  end
 end
