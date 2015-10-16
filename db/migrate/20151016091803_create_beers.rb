ef38679ff63a799469b1c6dafaf9965514463a91
Samuel Monroe <spat.monroe@gmail.com>
2015-10-16 12:38:17 +0200

    Nouveau schéma db

--- db/migrate/20151016091803_create_beers.rb
+++ db/migrate/20151016091803_create_beers.rb
@@ -1,6 +1,10 @@
 class CreateBeers < ActiveRecord::Migration
   def change
     create_table :beers do |t|
+      t.string :name
+      t.integer :degree
+      t.text :description
+      t.text :story
 
       t.timestamps null: false
     end
