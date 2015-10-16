b39b9677685bce704520955a4c416a4d16747669
Samuel Monroe <spat.monroe@gmail.com>
2015-10-16 12:37:43 +0200

    Début modélisation d'une Beer avec tests

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: beers
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  degree      :integer
+#  description :text
+#  story       :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Beer < ActiveRecord::Base
+
+  validates :name, :presence => true
+  validates :degree, :presence => true
+
 end
