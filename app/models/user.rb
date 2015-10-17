cece7f01beccfcef6150172ed148b921be6e0146
MRiffon <m.riffon@students.ephec.be>
2015-10-17 16:01:44 +0200

    Changement concernant le password et l'email

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,15 @@
 class User < ActiveRecord::Base
 
   validates :login, presence: true,
+                    #length: { within: 6..25 }
                     length: { minimum: 6 , maximum: 25 }
+
+  validates :password, presence: true,
+                       length: { within: 6..40 }
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true,
+                    format: {with: email_regex},
+                    uniqueness: {case_sensitive: false}
 end
