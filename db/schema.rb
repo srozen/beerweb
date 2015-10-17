e59928ef14655eed5c6f424455efe595126689c7
MRiffon <m.riffon@students.ephec.be>
2015-10-17 16:01:08 +0200

    Ajout index sur l'email

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20151015154006) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["login"], name: "index_users_on_login", unique: true
 
 end
