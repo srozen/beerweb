e8b79756def6e3087c88117bf79f2a3facbb5585
maxime9540 <maxime9540.grenier@hotmail.com>
2015-10-20 16:05:35 +0200

    Ajout CSS Page Index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,11 @@ class PagesController < ApplicationController
   def index
 
   end
-  
+  def collection
+	@titre = "Collection"
+  end
+
+  def contact
+	 @titre = "Contact"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#index'
-
+  #root 'pages#index'
+  get '/catalogue', :to => 'pages#catalogue'
+  root :to => 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
