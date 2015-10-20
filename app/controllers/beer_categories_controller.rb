d6ba578d0bee8d22ac2008209e3120f931006c31
Samuel Monroe <spat.monroe@gmail.com>
2015-10-20 22:32:03 +0200

    Le catalogue fournit une version JSON (API Android)

--- app/controllers/beer_categories_controller.rb
+++ app/controllers/beer_categories_controller.rb
@@ -3,6 +3,10 @@ class BeerCategoriesController < ApplicationController
   # Utilise la méthode search du Beer pour renvoyer des bières
   def index
     @beers = Beer.search(params[:name])
-  end
 
+    respond_to do |format|
+        format.html
+        format.json { render json: @beers }
+    end
+  end
 end
