bce4204196abfefd5c245cf24ba16ec5a2206ca6
Samuel Monroe <spat.monroe@gmail.com>
2015-10-20 21:43:00 +0200

    Méthode index pour affichage du catalogue

--- app/controllers/beer_categories_controller.rb
+++ app/controllers/beer_categories_controller.rb
@@ -1,7 +1,8 @@
 class BeerCategoriesController < ApplicationController
 
+  # Utilise la méthode search du Beer pour renvoyer des bières
   def index
-    @beers = Beer.all
+    @beers = Beer.search(params[:name])
   end
 
 end
