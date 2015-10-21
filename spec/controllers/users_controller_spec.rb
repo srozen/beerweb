ad024f33b59914f7a8cd326888381a5a43baef01
Samuel Monroe <spat.monroe@gmail.com>
2015-10-21 14:20:38 +0200

    Avancement sur l'inscription

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,15 +6,50 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET 'new'" do
 
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
     it "devrait réussir" do
       get 'new'
-      response.should be_success
+      expect(response).to be_success
     end
 
     it "devrait avoir le bon titre" do
       get 'new'
-      response.should have_selector("title", :content => "Register")
+      expect(response).to have_selector("title", :content => "Register")
     end
 
+    it "devrait trouver le bon utilisateur" do
+      get :show, :id => @user
+      expect(assigns(:user)).to == @user
+    end
+
+    it "devrait inclure le login de l'utilisateur" do
+      get :show, :id => @user
+      expect(response).to have_selector("h1", :content => @user.login)
+    end
+
+    it "devrait avoir une image de profil" do
+      get :show, :id => @user
+      expect(response).to have_selector("h1>img", :class => "gravatar")
+    end
+  end
+
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it "devrait réussir" do
+      get :show, :id => @user
+      expect(response).to be_success
+    end
+
+    it "devrait trouver le bon utilisateur" do
+      get :show, :id => @user
+      expect(assigns(:user)) == @user
+    end
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 # En utilisant le symbole ':user', nous faisons que
 # Factory Girl simule un modèle User.
-Factory.define :user do |user|
-  user.nom                   "Michael Hartl"
-  user.email                 "mhartl@example.com"
-  user.pwd                   "foobar"
-  user.pwd_confirmation      "foobar"
+FactoryGirl.define do
+  factory :user do |user|
+    user.login                  "MichaelHartl"
+    user.email                  "mhartl@example.com"
+    user.pwd                    "foobar"
+    user.pwd_confirmation       "foobar"
+    user.firstName              "Samuel"
+    user.lastName               "Monroe"
+  end
 end
