99d0c62ca65cebdb9156ab2ef6577cbb424e2187
Samuel Monroe <spat.monroe@gmail.com>
2015-10-21 08:17:46 +0200

    Les entrées du catalogue forment un lien vers le SHOW d'une bière

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :beers
+  resources :beer_categories
 
   # Example resource route with options:
   #   resources :products do
