70f2667ff1bbf74d7ef210dd8e87e5cfb3d8a1c0
Samuel Monroe <spat.monroe@gmail.com>
2015-10-21 10:31:05 +0200

    Ajout salt aux users, Mick touche rien

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016091803) do
+ActiveRecord::Schema.define(version: 20151021083027) do
 
   create_table "beer_categories", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20151016091803) do
     t.date     "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "salt"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
