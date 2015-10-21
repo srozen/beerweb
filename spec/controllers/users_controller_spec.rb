a390c14d8f5bcbe4a5b38819ab52d0009982d86b
Samuel Monroe <spat.monroe@gmail.com>
2015-10-21 14:26:28 +0200

    Test dupliqué, et tests inutiles supprimésé

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -20,20 +20,6 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to have_selector("title", :content => "Register")
     end
 
-    it "devrait trouver le bon utilisateur" do
-      get :show, :id => @user
-      expect(assigns(:user)).to == @user
-    end
-
-    it "devrait inclure le login de l'utilisateur" do
-      get :show, :id => @user
-      expect(response).to have_selector("h1", :content => @user.login)
-    end
-
-    it "devrait avoir une image de profil" do
-      get :show, :id => @user
-      expect(response).to have_selector("h1>img", :class => "gravatar")
-    end
   end
 
   describe "GET 'show'" do
@@ -51,5 +37,6 @@ RSpec.describe UsersController, type: :controller do
       get :show, :id => @user
       expect(assigns(:user)) == @user
     end
+
   end
 end
