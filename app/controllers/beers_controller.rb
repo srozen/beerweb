f61ee1ba91f76bb0dc5306cf3ba85c021c6e5d16
Samuel Monroe <spat.monroe@gmail.com>
2015-10-23 15:45:07 +0200

    Rendu JSON d'une bière et de SA catégorie associée

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,14 +1,18 @@
 class BeersController < ApplicationController
-
   ##
   # Renvoie une entité Beer sur base d'un id
   # @param id :integer
   #
   def show
     @beer = Beer.find(params[:id])
+    @beer_category = BeerCategory.find_by_id(@beer.beer_category.id)
     respond_to do |format|
       format.html
-      format.json { render json: @beer }
+      format.json {
+        render :json => {
+          :beer => @beer, :category => @beer_category
+        }
+      }
     end
   end
 
