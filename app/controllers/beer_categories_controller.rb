1d4db18d4fdb6b0748e355c72eb1984340e0e213
Samuel Monroe <spat.monroe@gmail.com>
2015-10-23 15:44:46 +0200

    Rendu JSON d'une catégorie et SES bières

--- app/controllers/beer_categories_controller.rb
+++ app/controllers/beer_categories_controller.rb
@@ -9,4 +9,17 @@ class BeerCategoriesController < ApplicationController
         format.json { render json: @beers }
     end
   end
+
+  def show
+    @category = BeerCategory.find(params[:id])
+    @category_beers = Beer.where(:beer_category_id => @category.id)
+
+    respond_to do |format|
+      format.html
+      format.json {render :json => {
+          :category => @category, :beers => @category_beers
+        }
+      }
+    end
+  end
 end
