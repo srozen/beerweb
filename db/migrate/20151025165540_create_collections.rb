368220b6e86c3568240fddbe2845bec44f669237
Samuel Monroe <spat.monroe@gmail.com>
2015-10-25 18:22:58 +0100

    Relations sur les migrations

--- db/migrate/20151025165540_create_collections.rb
+++ db/migrate/20151025165540_create_collections.rb
@@ -2,6 +2,8 @@ class CreateCollections < ActiveRecord::Migration
   def change
     create_table :collections do |t|
 
+      t.belongs_to :user, index: true
+
       t.timestamps null: false
     end
   end
--- db/migrate/20151025165610_create_reviews.rb
+++ db/migrate/20151025165610_create_reviews.rb
@@ -2,6 +2,9 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
 
+      t.belongs_to :collection, index: true
+      t.belongs_to :beer, index: true
+
       t.timestamps null: false
     end
   end
