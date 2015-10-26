395d82d8d8785ef916fc710e2bbdc1d114bbacec
Samuel Monroe <spat.monroe@gmail.com>
2015-10-26 16:25:19 +0100

    Ajout de champs

--- db/migrate/20151025165610_create_reviews.rb
+++ db/migrate/20151025165610_create_reviews.rb
@@ -2,6 +2,9 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
 
+      t.float :note
+      t.text :comment
+
       t.belongs_to :collection, index: true
       t.belongs_to :beer, index: true
 
