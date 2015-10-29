cee0edacc5a949d40fcbfec6c6c85f344e124984
tof360 <chris_1232@hotmail.com>
2015-10-29 14:44:35 +0100

    Config db

--- db/migrate/20151027121741_create_deals.rb
+++ db/migrate/20151027121741_create_deals.rb
@@ -1,6 +1,15 @@
 class CreateDeals < ActiveRecord::Migration
   def change
     create_table :deals do |t|
+		 t.string :name
+		 t.string :nameBeer
+		 t.text :description
+		 t.string :categorie
+		 t.datetime :datedebut
+		 t.datetime :datefin
+		 t.float :prix
+		 t.int :reference
+		 t.float :reduction
 
       t.timestamps null: false
     end
