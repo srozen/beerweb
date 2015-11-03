de42d9e1d76b70f9e15d625b091646620506d19f
maxime9540 <maxime9540.grenier@hotmail.com>
2015-11-03 16:01:04 +0100

    Ajout du controller pour les avis

---
+++ app/controllers/avis_controller.rb
@@ -0,0 +1,16 @@
+class AvisController < ApplicationController
+
+    def show
+      @beer = Beer.find(params[:id])
+
+      @reviews = Review.all
+      @nbReviews= Review.where("beer_id = ?", @beer.id) 
+      
+      @collections = Collection.all
+      @users = User.all
+      @titre = "Reviews"
+    end
+
+
+
+end
