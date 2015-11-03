9359ccf035df5ace5b5abeed3c2259eb28214e52
maxime9540 <maxime9540.grenier@hotmail.com>
2015-11-03 13:02:48 +0100

    Ajout de deux variables pour la vue bière

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,10 +5,14 @@ class BeersController < ApplicationController
   # @param id :integer
   #
   def show
+    @reviews = Review.all
+    @nbReviews= Review.where("beer_id = ?", params[:id])
+
     @beer = Beer.find(params[:id])
     respond_to do |format|
       format.html
       format.json { render json: @beer }
+   
     end
   end
 
