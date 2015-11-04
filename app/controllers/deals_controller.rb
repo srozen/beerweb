d4ff344074affe64e1170d91dc0965c80c2c747a
tof360 <chris_1232@hotmail.com>
2015-11-04 18:42:16 +0100

    Gestion des bons plans: controller new et destroy

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,5 +1,17 @@
 class DealsController < ApplicationController
   def index
     @deals = Deal.all
+	@title = "Bons plans"
+  end
+  
+  def new
+	@deals = Deal.new
+	@title = "Nouveau bon plan"
+  end
+  
+  def destroy
+    Deal.find(params[:id]).destroy
+    flash[:success] = "Deal destroy"
+    redirect_to deals_url
   end
 end
