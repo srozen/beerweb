c803cf12e77e0454c7102e1a9f3fbc73b12b1fc7
Samuel Monroe <spat.monroe@gmail.com>
2015-11-11 14:44:08 +0100

    L'affichage de la collection répond à l'API

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,7 +1,36 @@
 class CollectionsController < ApplicationController
   def show
-    @collection = Collection.find(user_id = params[:id])
 
+    if !params[:userId].nil?
+      userid = params[:userId]
+    else
+      userid = params[:id]
+    end
+
+    @collection = Collection.find(user_id = userid)
     @beers = Beer.all
+
+
+    @reviews = @collection.reviews
+    @collection_beers = []
+    @collection_reviews = []
+
+    @reviews.each do |review|
+      @collection_beers << review.beer
+      @collection_reviews << review
+    end
+
+    respond_to do |format|
+      format.html
+      format.json {render :json => {
+          :beers => @collection_beers,
+          :reviews => @collection_reviews
+        }
+      }
+      format.csv do
+        headers['Content-Disposition'] = "attachment; filename=\"beer_collection.csv\""
+        headers['Content-Type'] ||= 'text/csv'
+      end
+    end
   end
 end
