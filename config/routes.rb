cb79c8f3862ea42929bad486d155a4523e1f6ed0
Samuel Monroe <spat.monroe@gmail.com>
2015-11-12 11:07:04 +0100

    Clean final avant présentation

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,7 @@ resources :password_resets, :only => [:new, :create, :edit, :update]
   #   get 'products/:id' => 'catalog#view'
   post '/api_catalogue', :to => 'beer_categories#index'
   get '/catalogue', :to => 'beer_categories#index'
+  post '/catalogue', :to => 'beer_categories#index'
 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
