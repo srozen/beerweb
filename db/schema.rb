0ec2d635d521a5322cc81def9e33f3dfd3a6ab03
Samuel Monroe <spat.monroe@gmail.com>
2015-11-12 16:15:03 +0100

    Petit ajout, la consultation de collection identifie son possesseur

--- db/schema.rb
+++ db/schema.rb
@@ -50,11 +50,6 @@ ActiveRecord::Schema.define(version: 20151111093520) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "friend_statuses", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "friendlists", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at", null: false
