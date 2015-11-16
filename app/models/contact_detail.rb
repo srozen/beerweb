347a852abdf70ba58d00dc540b76fb4ae72ea370
Samuel Monroe <spat.monroe@gmail.com>
2015-11-16 19:09:19 +0100

    Les détails de contact sont reliées aux deux modèles

--- app/models/contact_detail.rb
+++ app/models/contact_detail.rb
@@ -2,18 +2,19 @@
 #
 # Table name: contact_details
 #
-#  id         :integer          not null, primary key
-#  telephone  :string
-#  website    :string
-#  street     :string
-#  number     :string
-#  zipcode    :string
-#  city       :string
-#  country    :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :integer          not null, primary key
+#  telephone     :string
+#  website       :string
+#  street        :string
+#  number        :string
+#  zipcode       :string
+#  city          :string
+#  country       :string
+#  beer_place_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
 #
 
 class ContactDetail < ActiveRecord::Base
-  belongs_to :Beerentity, :polymorphic => true
+  belongs_to :beer_place, :polymorphic => true
 end
