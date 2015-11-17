a3d397432c30a7efb421ad4f0f16e0428ddb3e3d
maxime9540 <maxime9540.grenier@hotmail.com>
2015-11-17 17:00:41 +0100

    Ajout du modèle friends_status

--- app/models/friend_status.rb
+++ app/models/friend_status.rb
@@ -1,12 +1,3 @@
-# == Schema Information
-#
-# Table name: friend_statuses
-#
-#  id         :integer          not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class FriendStatus < ActiveRecord::Base
-  has_and_belongs_to_many :friends
+	 has_and_belongs_to_many :users
 end
