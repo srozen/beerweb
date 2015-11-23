bfb06743faf483b76ed82c683fb7d418fb438100
Samuel Monroe <spat.monroe@gmail.com>
2015-11-23 17:54:22 +0100

    Script partagé pour création des users

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :deploy_to, '/home/deploy/beerweb'
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/new_sip_user.sh}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
