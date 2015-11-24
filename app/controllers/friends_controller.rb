e3903a92b6096e0c07c69fd391da24505ff2ec3d
Samuel Monroe <spat.monroe@gmail.com>
2015-11-24 12:44:24 +0100

    Liste d'ami en API

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,10 +1,7 @@
 class FriendsController < ApplicationController
 
   before_filter :authenticateFriends, :only => [:show]
-  ##
-  # Renvoie une entité Beer sur base d'un id
-  # @param id :integer
-  #
+
   def show
 
     if !params[:userId].nil?
@@ -12,11 +9,18 @@ class FriendsController < ApplicationController
     else
       userid = params[:id]
     end
-      
+
     @friendlists = Friendlist.all
     @friendlistsUser = Friendlist.find(user_id = userid)
     @friends = Friend.where("friendlist_id = ?", @friendlistsUser.id)
     @users = User.all
+    respond_to do |format|
+      format.json {
+        render :json => {
+          :friends => @friends
+        }
+      }
+    end
   end
 
 
@@ -25,9 +29,9 @@ class FriendsController < ApplicationController
     @users = User.paginate(:page => params[:page])
     @friendlists = Friendlist.find(user_id = current_user.id)
     if !params[:addFriend].blank?
-      @friends = Friend.new(:user_id => params[:addFriend], :friendlist_id => @friendlists.user_id )       #id de l'ami , friend list user en cours       
+      @friends = Friend.new(:user_id => params[:addFriend], :friendlist_id => @friendlists.user_id )       #id de l'ami , friend list user en cours
       @friends.save
-      
+
       @friend_statuses = Friend.find_by(user_id: @friendlists.user_id, friendlist_id:  params[:addFriend])
       if(@friend_statuses)
         @status = FriendStatus.new(:status => "ami", :friends_id => params[:addFriend] , :users_id => @friendlists.user_id)
@@ -39,10 +43,9 @@ class FriendsController < ApplicationController
 end
 
 
-private 
+private
 
   def authenticateFriends
     @user = User.find(params[:id])
-    deny_access_friends unless current_user?(@user) || isFriend? 
+    deny_access_friends unless current_user?(@user) || isFriend?
   end
-  
